e8779af28f338b6d9c0f5684ef4a972c5e30c5e0
Nguyen Minh Tuan <tnowad@gmail.com>
2023-11-25 13:08:17 +0700

    feat(plugin): comment unused plugin

--- plugins/community.lua
+++ plugins/community.lua
@@ -9,34 +9,34 @@ return {
   { import = "astrocommunity.media.vim-wakatime" },
 
   -- Editing Support
-  { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  -- { import = "astrocommunity.editing-support.todo-comments-nvim" },
 
   -- Frontend & NodeJS
-  { import = "astrocommunity.pack.typescript-all-in-one" },
+  -- { import = "astrocommunity.pack.typescript-all-in-one" },
   -- { import = "astrocommunity.pack.tailwindcss" },
   -- { import = "astrocommunity.pack.html-css" },
   -- { import = "astrocommunity.pack.prisma" },
   -- { import = "astrocommunity.pack.vue" },
 
   -- Configuration Languages
-  { import = "astrocommunity.pack.markdown" },
-  { import = "astrocommunity.pack.json" },
-  { import = "astrocommunity.pack.yaml" },
-  { import = "astrocommunity.pack.toml" },
+  -- { import = "astrocommunity.pack.markdown" },
+  -- { import = "astrocommunity.pack.json" },
+  -- { import = "astrocommunity.pack.yaml" },
+  -- { import = "astrocommunity.pack.toml" },
 
   -- Backend
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.go" },
-  { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.python" },
-  { import = "astrocommunity.pack.java" },
+  -- { import = "astrocommunity.pack.lua" },
+  -- { import = "astrocommunity.pack.go" },
+  -- { import = "astrocommunity.pack.rust" },
+  -- { import = "astrocommunity.pack.python" },
+  -- { import = "astrocommunity.pack.java" },
 
   -- Miscellaneous
-  { import = "astrocommunity.pack.docker" },
-  { import = "astrocommunity.pack.bash" },
+  -- { import = "astrocommunity.pack.docker" },
+  -- { import = "astrocommunity.pack.bash" },
 
-  -- Workflow and Completion
-  { import = "astrocommunity.completion.codeium-vim" },
-  { import = "astrocommunity.workflow.bad-practices-nvim" },
-  { import = "astrocommunity.workflow.hardtime-nvim" },
+  -- -- Workflow and Completion
+  -- { import = "astrocommunity.completion.codeium-vim" },
+  -- { import = "astrocommunity.workflow.bad-practices-nvim" },
+  -- { import = "astrocommunity.workflow.hardtime-nvim" },
 }
