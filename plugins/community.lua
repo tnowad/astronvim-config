89e8e58dc4e1c08c732a89ce7ebf59f0596b40ce
Nguyen Minh Tuan <tnowad@gmail.com>
2023-11-28 01:22:49 +0700

    feat(plugin): add lua

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,27 +1,21 @@
 return {
   "AstroNvim/astrocommunity",
 
-  -- Frontend & NodeJS
+  { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.typescript" },
+
   -- { import = "astrocommunity.pack.tailwindcss" },
   -- { import = "astrocommunity.pack.html-css" },
   -- { import = "astrocommunity.pack.prisma" },
   -- { import = "astrocommunity.pack.vue" },
-
-  -- Configuration Languages
   -- { import = "astrocommunity.pack.markdown" },
   -- { import = "astrocommunity.pack.json" },
   -- { import = "astrocommunity.pack.yaml" },
   -- { import = "astrocommunity.pack.toml" },
-
-  -- Backend
-  -- { import = "astrocommunity.pack.lua" },
   -- { import = "astrocommunity.pack.go" },
   -- { import = "astrocommunity.pack.rust" },
   -- { import = "astrocommunity.pack.python" },
   -- { import = "astrocommunity.pack.java" },
-
-  -- Miscellaneous
   -- { import = "astrocommunity.pack.docker" },
   -- { import = "astrocommunity.pack.bash" },
 }
